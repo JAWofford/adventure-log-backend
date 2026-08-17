33c33859c51774c127f3af67ee9305001a7ebcf3
JWofford <118766665+JAWofford@users.noreply.github.com>
2026-08-17 10:29:44 -0500

    Intial Spring Boot project structure

---
+++ src/main/java/com/jwofford/adventure_log_backend/AdventureLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jwofford.adventure_log_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureLogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwofford/adventure_log_backend/AdventureLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwofford.adventure_log_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureLogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
